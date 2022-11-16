8fcd15285025f9358a0457f203acebd25e2915db
MateuszJas <pphu.jaskolka@hotmail.com>
2022-11-16 21:56:10 +0100

    Dodaj pliki projektów.

---
+++ DoWhile1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhile1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x ;
+
+            do
+            {
+                Console.WriteLine("Podaj liczbę 2<:x:>3");
+                x = System.Convert.ToDouble(Console.ReadLine());
+
+                
+            } while (x <= 2 | x >= 3);
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
